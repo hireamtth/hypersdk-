7ca060668ec4261cf06da4cf9735b338c170c9e1
hireamtth <prajwalhiremath021@gmail.com>
2024-09-03 23:34:10 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "BLOCK"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "BLOCKCHAIN"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "BK"
 )
 
 var ID ids.ID
